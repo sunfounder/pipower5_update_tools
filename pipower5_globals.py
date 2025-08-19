de1a5cd6fc8be7a483b25624451c876c0363c539
Lovmong <1967471283@qq.com>
2025-08-19 17:18:55 +0800

    fix reg addrs of data

--- pipower5_globals.py
+++ pipower5_globals.py
@@ -6,20 +6,21 @@
 APP_I2C_ADDR = 0x5c
 BOOT_I2C_ADDR = 0x5d
 
-#
-BOARD_ID_REG_ADDR = 3 # 3: id_h, 4: id_l
-                      # Fusion Hat: 1908
-APP_VERSION_REG_ADDR = 5 # 5: major, 6: minor, 7, patch                                                                     |
-BOOT_VERSION_REG_ADDR = 207 # 207: major, 208: minor, 209, patch
-FACTORY_VERSION_REG_ADDR = 204 # 204: major, 205: minor, 206, patch
-MIAN_ENTRY_REG_ADDR = 200 # 200~203: bit3 ~ bit0
 
-#
+# ENDIAN = 'big'
+BOARD_ID_REG_ADDR = 140 # 140: id_h, 141: id_l
+                      # PiPower 5: 2602
+APP_VERSION_REG_ADDR = 128 # 128: major, 129: minor, 130, patch                                                                     |
+BOOT_VERSION_REG_ADDR = 173 # 173: major, 174: minor, 175, patch
+FACTORY_VERSION_REG_ADDR = 176 # 176: major, 177: minor, 178, patch
+MIAN_ENTRY_REG_ADDR = 183 # 183~186: bit3 ~ bit0
+
+# ENDIAN = 'big'
 BOOT_VERSION_REG_ADDR_BOOT = 0
 FACTORY_VERSION_REG_ADDR_BOOT = 3
 APP_VERSION_REG_ADDR_BOOT = 6
 BOOT_MODE_BOOT = 9
-MIAN_ENTRY_REG_ADDR_BOOT = 10
+MIAN_ENTRY_REG_ADDR_BOOT = 10 
 
 #
 FACTORY_APP_MODE = 0
